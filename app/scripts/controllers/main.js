ae7f3142ce1ea9967c37d3e4ef5e344a7ce8a1a9
Layoneth <layoneth14@gmail.com>
2014-04-27 21:49:37 -0500

    casi terminado.
    El ultimo cambio del día, trabajado el controlador.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,12 +1,35 @@
-'use strict';
+'use strict',
 
 angular.module('preguntasApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma',
-      'ui.router'
-    ];
+  .controller('MainCtrl',[ '$scope','$http', function ($scope, $http) {
+    
+    $scope.CrearPregunta = function () {
+    	var PreguntaEnviada = "pregunta =" + $scope.pregunta + "& categoria =" + $scope.categoria
+    	var Respuestas = "Opcion A = " + $scope.opcionA + "Opcion B = " + $scope.opcionB + "Opcion C = " + $scope.opcionC + "Opcion D = " + $scope.opcionD
+    		console.log(PreguntaEnviada);
+    		console.log(Respuestas);
 
-  });
+    	$http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+    	$http.post('http://192.168.1.157:8000' ).
+			success(function(){ 
+			$http.get('http://192.168.1.157:8000/notas/').
+						success(function (preg) {
+							console.log("Pregunta creada");
+							console.log(preg)
+
+							$scope.pregunta = preg;
+							})
+				    }).error(function() {
+				      console.log("No se pudo conectar a su servidor");
+				    });
+		};
+
+    	$scope.PreguntaAñadida = 
+    		$http.get('http://192.168.1.186:8000').
+    		success(function (data){
+      		$scope.QuestionSave = data;
+      		}).
+      		error(function () {
+  			console.log('no se pudo conectar a Django')    	
+      	});
+}])
